297fbdb4ffce27d726268fb023a4fd610d1e3be9
David Gallmeier <david.gallmeier@supply.com>
2020-12-16 15:25:56 -0500

    Update k8s client

--- ingresses.go
+++ ingresses.go
@@ -7,11 +7,24 @@ import (
 )
 
 func (c *Client) ListIngresses(namespace string) ([]v1beta1.Ingress, error) {
-	ingresses, err := c.K8sClientSet.ExtensionsV1beta1().Ingresses(namespace).List(context.TODO(), metav1.ListOptions{})
+	ingresses, err := c.K8sClientSet.
+		ExtensionsV1beta1().
+		Ingresses(namespace).
+		List(
+			context.TODO(),
+			metav1.ListOptions{})
+
+	filteredIngresses := make([]v1beta1.Ingress, 0)
+	for _, ingress := range ingresses.Items {
+		if(ingress.Spec.Rules[0].IngressRuleValue.HTTP.Paths[0].Backend.ServiceName == "service-nest-dev") {
+			filteredIngresses = append(filteredIngresses, ingress)
+		}
+	}
 
 	if(err != nil) {
 		return nil, err
 	}
 
-	return ingresses.Items, nil
+	//return ingresses.Items, nil
+	return filteredIngresses, nil
 }
--- ingresses_test.go
+++ ingresses_test.go
@@ -19,3 +19,18 @@ func TestClient_ListIngresses(t *testing.T) {
 		t.Errorf("error getting ingresses: %s", ingressesError)
 	}
 }
+
+func TestClient_ListIngressesFiltered(t *testing.T) {
+	namespace := "supply-ferguson-poc"
+	clientset, clientsetError := NewClient("https://feidsupplyaks001-2bd25411.hcp.eastus2.azmk8s.io:443", "/Users/d.gallmeier/.kube/config")
+	ingresses, ingressesError := clientset.ListIngresses(namespace)
+
+	fmt.Printf("There are %d ingresses in the namespace: %s\n", len(ingresses), namespace)
+
+	if(clientsetError != nil) {
+		t.Errorf("error creating client: %s", clientsetError)
+	}
+	if(ingressesError != nil) {
+		t.Errorf("error getting ingresses: %s", ingressesError)
+	}
+}
