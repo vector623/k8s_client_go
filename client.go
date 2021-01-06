264433cc2e77e20c61320516cd801fc3cd6b9781
David Gallmeier <david.gallmeier@supply.com>
2021-01-06 11:28:03 -0500

    Update config init

--- client.go
+++ client.go
@@ -3,9 +3,11 @@ package k8s_client_go
 import (
 	"errors"
 	"gopkg.in/yaml.v2"
+	"io/ioutil"
 	"k8s.io/client-go/kubernetes"
 	_ "k8s.io/client-go/plugin/pkg/client/auth/azure"
 	"k8s.io/client-go/tools/clientcmd"
+	"os"
 )
 
 // HostURL - Default Hashicups URL
@@ -38,25 +40,56 @@ func NewClient(apihost, kubeconfig string) (*Client, error) {
 func GenerateKubeConf(name, serverUrl, clientCertificateData, clientKeyData, token  string) (string, error) {
 	aksCreds := AksCreds{
 		Name: name,
-		User: User{
+		User: struct {
+			ClientCertificateData string `yaml:"client-certificate-data"`
+			ClientKeyData         string `yaml:"client-key-data"`
+			Token                 string
+		}{
 			ClientCertificateData: clientCertificateData,
-			ClientKeyData:         clientKeyData,
-			Token:                   token,
+			ClientKeyData: clientKeyData,
+			Token: token,
 		},
 	}
 
 	aksCredsYaml, _ := yaml.Marshal(aksCreds)
 	aksCredsYamlStr := string(aksCredsYaml)
-	return aksCredsYamlStr, errors.New("tba")
+
+	return aksCredsYamlStr, nil
 }
 
-type User struct {
-	ClientCertificateData string
-	ClientKeyData         string
-	Token                 string
+func GenerateKubeConfTempFile(name, serverUrl, clientCertificateData, clientKeyData, token string) (*os.File, error) {
+	aksCreds := AksCreds{
+		Name: name,
+		User: struct {
+			ClientCertificateData string `yaml:"client-certificate-data"`
+			ClientKeyData         string `yaml:"client-key-data"`
+			Token                 string
+		}{
+			ClientCertificateData: clientCertificateData,
+			ClientKeyData: clientKeyData,
+			Token: token,
+		},
+	}
+
+	aksCredsYaml, _ := yaml.Marshal(aksCreds)
+
+	tmpFile, err := ioutil.TempFile(os.TempDir(), "prefix-")
+	if err != nil {
+		return nil, errors.New("Cannot create temporary file")
+	}
+
+	if _, err = tmpFile.Write(aksCredsYaml); err != nil {
+		return nil, errors.New("Failed to write to temporary file")
+	}
+
+	return tmpFile, nil
 }
 
 type AksCreds struct {
 	Name string
-	User User
+	User struct {
+		ClientCertificateData string `yaml:"client-certificate-data"`
+		ClientKeyData         string `yaml:"client-key-data"`
+		Token                 string
+	}
 }
