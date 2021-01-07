9ddea542e986760844e2402abd181255883f8b90
David Gallmeier <david.gallmeier@supply.com>
2021-01-07 16:51:52 -0500

    Save changes

--- ingresses_test.go
+++ ingresses_test.go
@@ -7,7 +7,7 @@ import (
 
 func TestClient_ListIngresses(t *testing.T) {
 	namespace := "supply-ferguson-poc"
-	clientset, clientsetError := NewClient("https://feidsupplyaks001-2bd25411.hcp.eastus2.azmk8s.io:443", "/Users/d.gallmeier/.kube/config.yml")
+	clientset, clientsetError := NewClientFromKubeFile("https://feidsupplyaks001-2bd25411.hcp.eastus2.azmk8s.io:443", "/Users/d.gallmeier/.kube/config.yml")
 	ingresses, ingressesError := clientset.ListIngresses(namespace)
 
 	fmt.Printf("There are %d ingresses in the namespace: %s\n", len(ingresses), namespace)
@@ -22,7 +22,7 @@ func TestClient_ListIngresses(t *testing.T) {
 
 func TestClient_ListIngressesFiltered(t *testing.T) {
 	namespace := "supply-ferguson-poc"
-	clientset, clientsetError := NewClient("https://feidsupplyaks001-2bd25411.hcp.eastus2.azmk8s.io:443", "/Users/d.gallmeier/.kube/config.yml")
+	clientset, clientsetError := NewClientFromKubeFile("https://feidsupplyaks001-2bd25411.hcp.eastus2.azmk8s.io:443", "/Users/d.gallmeier/.kube/config.yml")
 	ingresses, ingressesError := clientset.ListIngresses(namespace)
 
 	fmt.Printf("There are %d ingresses in the namespace: %s\n", len(ingresses), namespace)
@@ -37,7 +37,7 @@ func TestClient_ListIngressesFiltered(t *testing.T) {
 
 func TestClient_ListIngressesSandbox(t *testing.T) {
 	namespace := "supply-ferguson-poc"
-	clientset, clientsetError := NewClient("https://feiseu2supplyaks001-92ae9238.hcp.eastus2.azmk8s.io:443", "~/go/src/k8s_client_go")
+	clientset, clientsetError := NewClientFromKubeFile("https://feiseu2supplyaks001-92ae9238.hcp.eastus2.azmk8s.io:443", "~/go/src/k8s_client_go")
 	ingresses, ingressesError := clientset.ListIngresses(namespace)
 
 	fmt.Printf("There are %d ingresses in the namespace: %s\n", len(ingresses), namespace)
